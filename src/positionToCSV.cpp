4d918845c361d4ceb0a7c6c77a7c551c3296225a
KnightCodder <knightcodder@gmail.com>
2024-04-17 10:40:31 +0530

    fixed some bugs

--- src/positionToCSV.cpp
+++ src/positionToCSV.cpp
@@ -4,41 +4,48 @@
 std::string Position::toCSV()
 {
     std::stringstream data;
+    board.moveGeneration();
 
     data << moveNum << ',' << board.turn << ',' << board.checks << ',' << board.castle[BOARD::whiteKingSideCastleIndex] << ',' << board.castle[BOARD::whiteQueenSideCastleIndex] << ',' << board.castle[BOARD::blackKingSideCastleIndex] << ',' << board.castle[BOARD::blackQueenSideCastleIndex] << ',' << board.fiftyMoves << ',' << board.materialBalance << ',' << eval;
 
-    for (auto piece : board.board)
+
+    for (int rank = 0; rank < 8; rank++)
     {
-        int WWatchers = 0, BWatchers = 0, EWatching = 0, WWatching = 0, BWatching = 0, KWatching = 0;
-
-        for (auto x : piece->watchers)
+        for (int file = 0; file < 8; file++)
         {
-            if (board.boardIndex(x.first)->color == COLOR::white)
-                WWatchers++;
-            else if (board.boardIndex(x.first)->color == COLOR::black)
-                BWatchers++;
-            else
-                std::cout << "error in watchers" << std::endl;
-        }
+            Piece *piece = board.boardIndex({rank, file});
 
-        for (auto x : piece->watchingSquare)
-        {
-            for (auto y: x.second)
+            int WWatchers = 0, BWatchers = 0, EWatching = 0, WWatching = 0, BWatching = 0, KWatching = 0;
+
+            for (auto x : piece->watchers)
             {
-                if (board.boardIndex(y)->color == board.turn && board.boardIndex(y)->pieceType == PIECE::KING)
-                    KWatching++;
-                else if (board.boardIndex(y)->color == COLOR::white)
-                    WWatching++;
-                else if (board.boardIndex(y)->color == COLOR::black)
-                    BWatching++;
-                else if (board.boardIndex(y)->color == COLOR::nothing)
-                    EWatching++;
+                if (board.boardIndex(x.first)->color == COLOR::white)
+                    WWatchers++;
+                else if (board.boardIndex(x.first)->color == COLOR::black)
+                    BWatchers++;
                 else
-                    std::cout << "error in watching square" << std::endl;
+                    std::cout << "error in watchers" << std::endl;
             }
-        }
 
-        data << ',' << (piece->pieceType == PIECE::PAWN) << ',' << (piece->pieceType == PIECE::KNIGHT) <<  ',' <<(piece->pieceType == PIECE::BISHOP || piece->pieceType == PIECE::QUEEN) << ',' << (piece->pieceType == PIECE::ROOK || piece->pieceType == PIECE::QUEEN) << ',' << (piece->pieceType == PIECE::KING) << ',' << piece->color << ',' << WWatchers << ',' << BWatchers << ',' << EWatching << ',' << WWatching << ',' << BWatching << ',' << KWatching;
+            for (auto x : piece->watchingSquare)
+            {
+                for (auto y : x.second)
+                {
+                    if (board.boardIndex(y)->color == board.turn && board.boardIndex(y)->pieceType == PIECE::KING)
+                        KWatching++;
+                    else if (board.boardIndex(y)->color == COLOR::white)
+                        WWatching++;
+                    else if (board.boardIndex(y)->color == COLOR::black)
+                        BWatching++;
+                    else if (board.boardIndex(y)->color == COLOR::nothing)
+                        EWatching++;
+                    else
+                        std::cout << "error in watching square" << std::endl;
+                }
+            }
+
+            data << ',' << (piece->pieceType == PIECE::PAWN) << ',' << (piece->pieceType == PIECE::KNIGHT) << ',' << (piece->pieceType == PIECE::BISHOP || piece->pieceType == PIECE::QUEEN) << ',' << (piece->pieceType == PIECE::ROOK || piece->pieceType == PIECE::QUEEN) << ',' << (piece->pieceType == PIECE::KING) << ',' << piece->color << ',' << WWatchers << ',' << BWatchers << ',' << EWatching << ',' << WWatching << ',' << BWatching << ',' << KWatching;
+        }
     }
 
     return data.str();
--- tests/test1.cpp
+++ tests/test1.cpp
@@ -8,9 +8,9 @@ int main()
 
     dataGenerator a(DATA_FILE::defaultInputFileName, DATA_FILE::defaultOutputFileName, DATA_FILE::defaultCacheFileName, DATA_FILE::latestRow);
 
-    a.setStartingRowNum(2);
+    // a.setStartingRowNum(2); // to read the first game
 
-    a.generateData(a.startingRowNum, 1);
+    a.generateData(a.startingRowNum, 100);
 
     return 0;
 }
